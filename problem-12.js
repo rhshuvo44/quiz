243201900d9f00eb77f787b1f6f0085a277e4451
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-02-19 22:40:08 +0600

    ss

--- problem-12.js
+++ problem-12.js
@@ -1,43 +1,42 @@
-//prime number
-const number = 6;
-if (number == 1) {
-    console.log("1 is neither prime nor composite number.");
-}else if (number > 1) {
-    for (let i = 2; i < number; i++) {
-        if (number % i == 0) {
-        var result=`${number} is a not prime number`;
-            break;
-        }else{
-            var result=`${number} is a prime number`;
+var matrix1 = [
+    [2,4],
+    [6,8],
+    [10,12]
+]
+
+var matrix2 = [
+    [1,3],
+    [5,7],
+    [9,11]
+]
+
+var resultMatrix = [];
+
+const row1 = matrix1.length;
+const col1= matrix1[0].length;
+
+const row2 = matrix2.length;
+const col2 = matrix2[0].length;
+if (row1 ==row2 && col1 == col2) {
+    for (let i =0; i<matrix1.length;i++) {
+             var emptyArray =[];
+        for (let j =0; j<matrix1[i].length;j++) {
+            var number1 = matrix1[i][j];
+            var number2 = matrix2[i][j]
+            var result = number1+number2;
+            emptyArray.push(result)
         }
-    }
-    console.log(result);
-}else {
-    console.log("The number is not a prime number.");
-}
-///first question ta chilo first 15 odd number addition
-var sum = 0;
-for (let i = 1; i <= 15; i++) {
-if (i%2!=0) {
-    sum=sum+i;
-}    
-}
-console.log(sum);
-//second question ta chilo first 20 even number multiplication
-var sum = 1;
-for (let i = 1; i <= 20; i++) {
-if (i%2==0) {
-    sum=sum*i;
-}    
-}
-console.log(sum);
-//third # - 
-var result =[]
-for (let i = 1; i < 10; i++) {
-    if (i % 2!=0) {
-       result.push('# ');
-    }else{
-        result.push('-');
+        resultMatrix.push(emptyArray)
     } 
+    
+    for (const resultMat of resultMatrix) {
+        var s= '';
+        for (const result of resultMat) {
+            s = s+result +'\t'
+        }
+        console.log(s);
+    }
 }
-console.log(result.join(' '));
+else{
+    console.log("Can not perform Addition. Invalid Dimension!");
+}
